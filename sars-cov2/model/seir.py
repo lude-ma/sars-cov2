c56b61183a772ec5e9f46945ab3db0007139ba22
Marvin Ludersdorfer <marvin.ludersdorfer@tum.de>
2020-04-30 23:46:55 +0200

    implemented SEIR model

--- sars-cov2/model/seir.py
+++ sars-cov2/model/seir.py
@@ -1,2 +1,13 @@
-#! /usr/bin/env python
+from numpy import array as vector
 
+
+def seir_model(beta, gamma, a):
+    def f(t, x):
+        S, E, I, R = x
+        return vector([
+            -beta*S*I,
+            beta*S*I - a*E,
+            a*E - gamma*I,
+            gamma*I
+        ])
+    return f
