5e393a647d8a942f1e64eb2678b65dca8cc22b82
Marvin Ludersdorfer <marvin.ludersdorfer@tum.de>
2020-05-02 14:50:17 +0200

    description of SEIR model

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Modelling CoViD-19
+
+Implementierung von epidemiologischen Modellen.
+
+## SEIR-Modell
+Das SEIR Modell ist ein Ansatz zur Beschreibung der Ausbreitung einer Ansteckenden Krankheit in einer Population.
+Es erweitert das SIR-Modell, indem berücksichtigt wird, dass ein Individuum nach seiner Ansteckung nicht sofort selbst infektiös ist.
+
+Als Quelle dient [SEIR-Modell](https://de.wikipedia.org/wiki/SEIR-Modell).
--- sars_cov2/model/seir.py
+++ sars_cov2/model/seir.py
@@ -1,7 +1,30 @@
+# coding=utf-8
 from numpy import array as vector
 
 
 def seir_model(beta, gamma, a):
+    """SEIR-Modell.
+    Beschreibt eine Population von N Individuen als
+        NS + NE + NI + NR = N
+    mit
+        S: Anteil der Anfälligen. Noch nicht infiziert. (engl. susceptible)
+        E: Anteil der Exponierten. Infiziert aber noch nicht infektiös.
+           (engl. exposed)
+        I: Anteil der Infektiösen. (engl. infectious)
+        R: Anteil der Erholten oder Verstorbenen. (engl. recovered/resistant)
+
+    :param beta: Transmissionsrate (Kontaktrate) [1/d]. Der Kehrwert ist die
+                 mittlere Zeit zwischen Kontakten.
+    :param gamma: Gesundungsrate [1/d]. Der Kehrwert ist die mittlere
+                  infektiöse Zeit.
+    :param a: Der Kehrwert ist die mittlere Latenzzeit [1/d]. Die mittlere
+              Latenzzeit ist die durchschnittliche Zeit, die ein Individuum in
+              der Gruppe E der Exponierten verbringt. Diese ist zu
+              unterscheiden von der mittleren Inkubationszeit, denn der Beginn
+              der Infektiösität muss nicht mit dem Beginn der Symptome
+              übereinstimmen.
+    :return: Nichtlineares System gewöhnlicher Differentialgleichungen.
+    """
     def f(t, x):
         S, E, I, R = x
         return vector([
