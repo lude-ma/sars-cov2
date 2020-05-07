2eda28d8bb413d0e64b1e23b671e37d44c3d0e15
Marvin Ludersdorfer <marvin.ludersdorfer@tum.de>
2020-05-07 20:48:00 +0200

    documented explicit Euler method

--- sars_cov2/utils/solver.py
+++ sars_cov2/utils/solver.py
@@ -1,10 +1,26 @@
+# coding=utf-8
+
+
 def euler_method(f, t0, x0, t1, h):
-    """Explicit Euler method."""
+    """Explizites Euler-Verfahren.
+    Einfachstes Verfahren zur numerischen Lösung eines Anfangswertproblems
+        x' = f(t, x), x(t0) = x0
+    durch Berechnung von
+        tk = t0 + k*h, k = 0, 1, 2, ...
+        xk+1 = xk + h*f(tk, xk), k + 0, 1, 2, ...
+
+    :param f: zu lösendes Anfangswertproblem x' = f(t, x), x(t0) = x0
+    :param t0: Anfangs-Zeitpunkt t0
+    :param x0: Anfangswert x0
+    :param t1: End-Zeitpunkt t1
+    :param h: Diskretisierungs-Schrittweite h > 0
+    :return: Liste von Approximationen an den diskreten Zeitpunkten k
+    """
     t = t0
     x = x0
-    a = [[t, x]]
+    approx = [[t, x]]
     for k in range(0, 1 + int((t1 - t0)/h)):
         t = t0 + k*h
         x = x + h*f(t, x)
-        a.append([t, x])
-    return a
+        approx.append([t, x])
+    return approx
